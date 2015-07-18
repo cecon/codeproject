dab81cb0f6d313f6feaf9a1d7b7c73bbdb26b0de
Clecio <clecio.sb@gmail.com>
2015-07-17 22:17:00 -0300

    Criado model e controller de client

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,23 @@
 |
 */
 
+
 $factory->define(CodeProject\User::class, function ($faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => str_random(10),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(CodeProject\Client::class, function ($faker) {
+    return [
+        'name' => $faker->name,
+        'responsible' => $faker->name,
+        'email' => $faker->email,
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'obs' => $faker->sentence,
     ];
 });
